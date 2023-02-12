fc19489a11bd8cc9b78df0a0bbe2fc874d430305
Maximilian Korte <maximilian.korte@adesso.de>
2023-02-12 13:28:43 +0100

    Add basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,37 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './ui/header/header.component';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HeaderComponent} from './ui/header/header.component';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatGridListModule} from "@angular/material/grid-list";
 import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
-import { DashboardButtonComponent } from './ui/dashboard-button/dashboard-button.component';
+import {DashboardButtonComponent} from './ui/dashboard-button/dashboard-button.component';
+import {DashboardComponent} from './pages/dashboard/dashboard.component';
+import {RouterOutlet} from "@angular/router";
+import {AppRoutingModule} from "./app-routing.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    DashboardButtonComponent
+    DashboardButtonComponent,
+    DashboardComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatGridListModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    RouterOutlet
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
